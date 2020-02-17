1248009460e848921bdcf9ee56b152ea2535f3b0
Seun Odewale <seunodewale.zone5@gmail.com>
2020-02-17 19:54:22 +0100

    fixed search records by different conditions

--- medic/urls.py
+++ medic/urls.py
@@ -3,12 +3,13 @@ from django.contrib import admin
 from django.contrib.auth import views as auth_views
 from django.urls import path, include
 from users import views as user_views
-from patient.views import RecordListView, RecordDetailView, RecordCreateView
+from patient.views import search, RecordListView, RecordDetailView, RecordCreateView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('user_signup/', user_views.signup, name='user_signup'),
     path('record/', RecordListView.as_view(), name='record'),
+    path('record/search/', search, name='search'),
     path('record/<int:pk>/', RecordDetailView.as_view(), name='record_detail'),
     path('record/new/', RecordCreateView.as_view(), name='new_record'),
     path('login/', auth_views.LoginView.as_view(template_name='users/login.html'), name='login'),
--- patient/views.py
+++ patient/views.py
@@ -8,6 +8,15 @@ from django.views.generic import ListView, DetailView, CreateView
 def home(request):
     return render(request, 'patient/home.html')
 
+def search(request):
+    records = Record.objects.order_by('-created_at')
+
+    if 'condition' in request.GET:
+        keywords = request.GET['condition']
+        if keywords:
+            records = records.filter(condition__iexact=keywords)
+
+    return render(request, 'patient/record_list.html', {'records': records})
 
 class RecordListView(ListView):
     model = Record
@@ -21,7 +30,7 @@ class RecordCreateView(LoginRequiredMixin, CreateView):
     model = Record
     fields = ['firstname', 'lastname', 'telephone', 'gender', 'address', 'condition', 'description']
 
-    def form_valid( self, form):
+    def form_valid(self, form):
         form.instance.created_by = self.request.user
         messages.success(self.request, f'Your record was added successfullly.')
         return super().form_valid( form)
